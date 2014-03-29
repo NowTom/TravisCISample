41ad95d4088ba956f2f181c9dc82fd06a7601435
Tomoaki Imai <imaitomoaki@gmail.com>
2014-03-29 17:29:50 +0900

    Initial Commit

---
+++ TravisCISample/TravisCISample.h
@@ -0,0 +1,13 @@
+//
+//  TravisCISample.h
+//  TravisCISample
+//
+//  Created by NowTom on 3/29/14.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TravisCISample : NSObject
+
+@end
---
+++ TravisCISample/TravisCISample.m
@@ -0,0 +1,13 @@
+//
+//  TravisCISample.m
+//  TravisCISample
+//
+//  Created by NowTom on 3/29/14.
+//
+//
+
+#import "TravisCISample.h"
+
+@implementation TravisCISample
+
+@end
